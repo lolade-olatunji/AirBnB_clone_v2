fc40de10452c717fb193a51bd69c2d4250db6b3e
Michael Raheem <abiolar544@gmail.com>
2024-01-18 00:20:44 +0100

    task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,15 @@
+-- Create the database if it doesn't exist
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
+-- Switch to hbnb_test_db
+USE hbnb_test_db;
+
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
+-- Grant privileges to the user on the database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
+-- Grant SELECT privilege on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
